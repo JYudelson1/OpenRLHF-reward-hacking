0e35715d1c37ee03ae4e374f754c58222091328f
Joey <jyudel@gmail.com>
2025-03-10 17:06:37 -0700

    fix sft bug

--- openrlhf/cli/train_sft.py
+++ openrlhf/cli/train_sft.py
@@ -100,7 +100,7 @@ def train(args):
     )
 
     # scheduler
-    num_update_steps_per_epoch = len(train_dataset) // args.train_batch_size
+    num_update_steps_per_epoch = len(train_data) // args.train_batch_size
     max_steps = math.ceil(args.max_epochs * num_update_steps_per_epoch)
 
     scheduler = get_scheduler(
