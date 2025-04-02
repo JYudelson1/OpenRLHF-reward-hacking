cc1398ffb6f429f6516048a678c10b79487e192a
Joey <jyudel@gmail.com>
2025-04-02 10:31:23 -0400

    Remove speculative decoding

--- openrlhf/trainer/ray/vllm_engine.py
+++ openrlhf/trainer/ray/vllm_engine.py
@@ -234,10 +234,6 @@ def create_vllm_engines(
                 mongo_uri=mongo_uri,
                 mongo_db_name=mongo_db_name,
                 mongo_collection_name=mongo_collection_name,
-                speculative_config={
-                    "model": "Qwen/Qwen2.5-Coder-1.5B",
-                    "num_speculative_tokens": 5,
-                }
             )
         )
 
