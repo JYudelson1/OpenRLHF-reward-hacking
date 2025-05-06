45f1e4db5416819d3d5db7cbefb41a0be1a0fb28
Joey <jyudel@gmail.com>
2025-05-06 17:46:31 -0400

    making serializable

--- openrlhf/trainer/ray/ppo_actor.py
+++ openrlhf/trainer/ray/ppo_actor.py
@@ -161,35 +161,6 @@ class ActorPPOTrainer(BasePPOTrainer):
             ray.get(refs)
 
         torch.distributed.barrier()
-        
-        # Create actor references to self for parallel operations
-        self.experience_maker_actor = None
-        self.trainer_actor = None
-        
-    def setup_ray_actors(self):
-        """Call this once before starting the training loop"""
-        # Define actor classes inside the method to access the trainer
-        
-        @ray.remote
-        class ExperienceMakerActor:
-            def __init__(self, experience_maker):
-                self.experience_maker = experience_maker
-                
-            def make_experience_list(self, prompts, **generate_kwargs):
-                return self.experience_maker.make_experience_list(prompts, **generate_kwargs)
-        
-        @ray.remote
-        class TrainerActor:
-            def __init__(self, trainer):
-                self.trainer = trainer
-                
-            def train(self, steps, pbar, args):
-                return self.trainer._train(steps, pbar, args)
-        
-        # Create actor instances
-        self.experience_maker_actor = ExperienceMakerActor.remote(self.experience_maker)
-        self.trainer_actor = TrainerActor.remote(self)  # Passing self as the trainer
-    
 
     def fit(
         self,
@@ -267,11 +238,6 @@ class ActorPPOTrainer(BasePPOTrainer):
             self._tensorboard.close()
             
     def _fit_one_off(self, args, prompts_dataloader, pretrain_dataloader, consumed_samples, num_update_steps_per_episodes):
-       
-        # Make sure actors are set up
-        if self.experience_maker_actor is None:
-            self.setup_ray_actors()    
-            
         num_rollouts_per_episodes = (
             num_update_steps_per_episodes
             * args.train_batch_size
@@ -306,12 +272,17 @@ class ActorPPOTrainer(BasePPOTrainer):
             )
 
             for (rollout_num, rand_prompts) in enumerate(self.prompts_dataloader):
-                rollout_ref = self.experience_maker_actor.make_experience_list.remote(
-                    rand_prompts, **self.generate_kwargs
+                rm_fn = self.experience_maker.reward_fn
+                abc_impl = self.experience_maker._abc_impl
+                del self.experience_maker.reward_fn
+                del self.experience_maker._abc_impl
+                rollout_ref = make_experience_list_remote.remote(
+                    self.experience_maker, rand_prompts, **self.generate_kwargs
                 )
-                
+                self.experience_maker.reward_fn = rm_fn
+                self.experience_maker._abc_impl = abc_impl
                 if rollout_num > 0:
-                    train_ref = self.trainer_actor.train.remote(steps, pbar, args)
+                    train_ref = train_remote.remote(self, steps, pbar, args)
                     rollout_experiences, _ = ray.get([rollout_ref, train_ref])
                     steps = steps + 1
                 else:
@@ -1190,10 +1161,10 @@ def custom_collate_fn(batch):
 ## Ray Remote Functions
 
 @ray.remote
-def make_experience_list_remote(experience_maker, prompts, **generate_kwargs):
+def make_experience_list_remote(experience_maker: RemoteExperienceMaker, prompts, **generate_kwargs):
     return experience_maker.make_experience_list(prompts, **generate_kwargs)
 
 @ray.remote
-def train_remote(trainer, steps, pbar, args):
+def train_remote(trainer: ActorPPOTrainer, steps, pbar, args):
     return trainer._train(steps, pbar, args)
 
