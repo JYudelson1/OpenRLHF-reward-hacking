3fe43e16d96fb95ba83d60b9ad764c8c9086abd2
Vladimir Ivanov <volodimir1024@gmail.com>
2025-06-02 21:02:34 +0100

    dev

--- openrlhf/utils/interface.py
+++ openrlhf/utils/interface.py
@@ -562,6 +562,7 @@ class AgentInterface(ABC):
             tools,
             chat_template_content_format,
             tokenizer,
+            model_config=model_config,
             trust_remote_code=model_config.trust_remote_code,
         )
 
