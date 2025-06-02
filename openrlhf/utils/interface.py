639c2edf06f5fd2303fd10316f6b3d5a22225eda
Vladimir Ivanov <volodimir1024@gmail.com>
2025-06-02 16:48:53 +0100

    dev

--- openrlhf/utils/interface.py
+++ openrlhf/utils/interface.py
@@ -456,7 +456,7 @@ class AgentInterface(ABC):
                 logger.error(f"Failed to upload conversations to MongoDB: {str(e)}")
 
     def _generate_chat_completions(self, messages: list[list[Message]]) -> Tuple[list[RequestOutput], List[bool]]|list[RequestOutput]:
-        if isinstance(self.llm_engine, vllm.LLM):
+        if isinstance(self.llm_engine, vllm.v1.engine.async_llm.AsyncLLM):
              #vLLM will apply its own truncation based on sampling_params.truncate_prompt_tokens if set
             return self.async_event_loop.run_until_complete(self._vllm_chat_with_truncation(
                 engine=self.llm_engine, 
