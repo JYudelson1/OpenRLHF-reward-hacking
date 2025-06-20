509cf0a2440074b02c35e156e4c7d81c9a60a0a2
Joey <jyudel@gmail.com>
2025-06-20 15:44:18 -0400

    using bundle indices

--- openrlhf/trainer/ray/vllm_engine.py
+++ openrlhf/trainer/ray/vllm_engine.py
@@ -246,12 +246,12 @@ def create_vllm_engines(
     for i in range(num_engines):
         bundle_indices = None
         if tensor_parallel_size > 1:
-            bundle_indices = list(range(i * tensor_parallel_size, (i + 1) * tensor_parallel_size))
+            bundle_indices = get_bundle_indices(shared_pg, i, tensor_parallel_size)
 
         scheduling_strategy = PlacementGroupSchedulingStrategy(
             placement_group=shared_pg,
             placement_group_capture_child_tasks=True,
-            placement_group_bundle_index=i * tensor_parallel_size,
+            placement_group_bundle_index=bundle_indices[0] if bundle_indices else i,
         )
 
         if num_engines >= num_total_actors:
