16d60bed451f7a4adf197b9aa45a72b42703ce24
Joey <jyudel@gmail.com>
2025-07-14 15:49:30 -0700

    typo

--- openrlhf/trainer/ppo_utils/experience_maker.py
+++ openrlhf/trainer/ppo_utils/experience_maker.py
@@ -1050,7 +1050,8 @@ class RemoteExperienceMaker(BaseExperienceMaker):
                     attention_mask = torch.tensor(attention_mask, device="cuda").unsqueeze(0)
                     action_mask = torch.tensor(action_masks, device="cuda").unsqueeze(0)
                     
-                    response_length = torch.tensor(num_actions, device="cuda", dtype=torch.float)
+                    response_length = torch.tensor(response_lengths, device="cuda", dtype=torch.float)
+                    num_actions = torch.tensor(num_actions, device="cuda", dtype=torch.float)
                     total_length = torch.tensor(packed_seq_lens, device="cuda", dtype=torch.float)
                     samples_list.append(
                         Samples(
