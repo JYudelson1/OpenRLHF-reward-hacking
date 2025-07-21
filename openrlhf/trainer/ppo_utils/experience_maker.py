39a72a89a94f5f884857cbd06086472c76c0f454
Joey <jyudel@gmail.com>
2025-07-21 16:18:39 -0700

    actually using length penalty

--- openrlhf/trainer/ppo_utils/experience_maker.py
+++ openrlhf/trainer/ppo_utils/experience_maker.py
@@ -595,14 +595,14 @@ class RemoteExperienceMaker(BaseExperienceMaker):
             lengths = lengths * -1.0 * getattr(args, "length_penalty", 0.0)
             lengths = torch.where(rewards_missing, torch.zeros_like(lengths), lengths)
             lengths = lengths.reshape(-1).to(device="cpu").chunk(len(experiences))
-
-            rewards = rewards + lengths
             
             for experience in experiences:
                 experience.info["extra_metrics/frac_mixed_reward_groups"] = torch.tensor([float(frac_nonzero_rows.item()) for _ in experience.sequences])
+                
+        rewards = (r + l for r, l in zip(rewards, lengths))
 
         # calculate return and advantages
-        for experience, reward in zip(experiences, rewards):
+        for experience, reward in zip(experiences, rewards, strict=True):
             experience = experience.to_device("cuda")
             reward = reward.to(device="cuda")
             num_actions = experience.info["num_actions"]
