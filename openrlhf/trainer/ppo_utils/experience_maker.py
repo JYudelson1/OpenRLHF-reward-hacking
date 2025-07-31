d1f4cad4f16ab55e578e8a2a8c5df8c7286b51f7
Joey <jyudel@gmail.com>
2025-07-31 14:04:00 -0700

    fix attempt

--- openrlhf/trainer/ppo_utils/experience_maker.py
+++ openrlhf/trainer/ppo_utils/experience_maker.py
@@ -1139,7 +1139,7 @@ class RemoteExperienceMaker(BaseExperienceMaker):
 
             print(f"Rank {rank} got to the barrier!")
             if self.strategy.ring_attn_group is not None:
-                torch.distributed.barrier(device_ids=list(self.strategy.ring_attn_group.ranks))
+                torch.distributed.barrier(device_ids=list(range(0, self.strategy.ring_attn_size)))
             else:
                 torch.distributed.barrier()
             torch.cuda.synchronize()
@@ -1156,7 +1156,7 @@ class RemoteExperienceMaker(BaseExperienceMaker):
             )
 
             if self.strategy.ring_attn_group is not None:
-                torch.distributed.barrier(device_ids=list(self.strategy.ring_attn_group.ranks))
+                torch.distributed.barrier(device_ids=list(range(0, self.strategy.ring_attn_size)))
             else:
                 torch.distributed.barrier()
             torch.cuda.synchronize()
@@ -1176,7 +1176,7 @@ class RemoteExperienceMaker(BaseExperienceMaker):
             )
 
             if self.strategy.ring_attn_group is not None:
-                torch.distributed.barrier(device_ids=list(self.strategy.ring_attn_group.ranks))
+                torch.distributed.barrier(device_ids=list(range(0, self.strategy.ring_attn_size)))
             else:
                 torch.distributed.barrier()
             torch.cuda.synchronize()
