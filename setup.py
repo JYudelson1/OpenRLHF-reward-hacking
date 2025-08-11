8b61905cf072664ce0de558d39b22eef516f6577
Vladimir Ivanov <96141489+astOwOlfo@users.noreply.github.com>
2025-08-11 12:21:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -74,8 +74,8 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=_fetch_requirements("requirements.txt"),
     extras_require={
-        "vllm": ["vllm==0.10.1"],
-        "vllm_latest": ["vllm>0.10.1"],
+        "vllm": ["vllm"],
+        "vllm_latest": ["vllm"],
     },
     python_requires=">=3.10",
     classifiers=[
